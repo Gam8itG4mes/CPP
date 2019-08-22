e7b2ccd0745ade516a258bc3a2f180869594488d
Gam8it <32150386+Gam8itG4mes@users.noreply.github.com>
2019-08-22 09:35:14 -0700

    Update integral.cpp

--- integral.cpp
+++ integral.cpp
@@ -23,9 +23,13 @@ int main() {
   }
   std::cout << std::endl;
 
+  //Integral for f(x) = x^2 | (b^3)/3 - (a^3)/3
   double anti = ((std::pow(upper,3)/3)-(std::pow(lower,3)/3));
   std::cout << "Actual integral value: " << anti << std::endl;
 
+  //estimate the integral f(x) = x^2 from 1 to 5
+  //by averaging areas under the curve where Area = (5-1)*x^2
+  //A = (b-a)*f(x)
   for (int i = 0; i < iterations; ++i)
   {
     float r = lower + static_cast<float>(rand()) / static_cast<float>(RAND_MAX/(upper-lower));
